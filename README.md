50a69e74f5e9c7fdd2c63986ceeca37febdb88d7
KaiErikNiermann <kai.niermann@gmail.com>
2024-04-05 18:04:18 +0200

    fix

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # flashcard-agg
+
+This repo allows for multiple people to work on an anki flashcard deck. 
+- The folders are titled with the topic of the flashcard ( e.g. `test` )
+- To get the aggregated deck of all flashcards [go to releases](https://github.com/KaiErikNiermann/flashcard-agg/releases/tag/cards) and simply find your deck, it should be called `u_test`  
+
+## How do I use this ? 
+
+The nice thing here is that the `concat.sh` script does most of the work, you simply 
+
+1. Clone the repo 
+
+```bash
+git clone https://github.com/KaiErikNiermann/flashcard-agg.git
+```
+
+2. Add your flashcards to the appropraite folder 
+
+3. If you don't want to contribute your flashcard you can locally generate the combined file by doing 
+```bash
+bash concat.sh
+```
+
+This generates a folder `release` which contains the combined cards 
+
+Otherwise you can simply push the changes which runs the script and adds the updated deck to [the release](https://github.com/KaiErikNiermann/flashcard-agg/releases/tag/cards)
+
+
--- concat.sh
+++ concat.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-blacklist=("header.txt" "concat.sh" ".github" ".git")
+blacklist=("header.txt" "concat.sh" ".github" ".git" "README.md" "release.sh" "release")
 
 function aggregate_cards() {
 	local dir_name=$1
 	local header=$2
-	local target_file="$dir_name.txt"
+	local allow_duplicates=$3
+	local target_file="u_$dir_name.txt"
 
     if [ -n "$header" ]; then
         touch "$target_file"
@@ -13,24 +14,40 @@ function aggregate_cards() {
 
         for FILE in *.txt; do
             if [ -s "$FILE" ] && [ "$FILE" != "$target_file" ]; then
-                comm -23 <(sort "$FILE") <(sort "$target_file") >> "$target_file"
+				if [ "$allow_duplicates" == "true" ]; then
+					 tail -n +3 "$FILE" >> "$target_file"
+				else
+					comm -23 <(sort "$FILE") <(sort "$target_file") >> "$target_file"
+				fi
             fi
         done
+		
+        cat "$target_file" 
+
+        cp "$target_file" "../release"
+
     else
         echo "No header content provided"
     fi
 }
 
 function run_all() {
+	local allow_duplicates=$1
 	local header_content=$(cat header.txt)
-    for d in */; do
+    mkdir -p release
+    
+    if [ "$allow_duplicates" = "" ]; then 
+        allow_duplicates="false"
+    fi
+    
+    for d in *; do
 		dir_name=$(basename "$d")
         if [[ ! " ${blacklist[@]} " =~ " ${d} " ]]; then
             cd "$d" || exit
-            aggregate_cards "$dir_name" "$header_content"
+            aggregate_cards "$dir_name" "$header_content" "$allow_duplicates"
             cd - || exit
         fi
     done
 }
 
-run_all
+run_all $1 
